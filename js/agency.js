ed903fd193a5f4b169591cf95343d6d8258e9f58
selique <judeudemolidor@hotmail.com>
2018-09-24 11:44:39 +0000

    adjust animation delay

--- js/agency.js
+++ js/agency.js
@@ -14,19 +14,18 @@
     //or any other method of not showing the spinner
   }
 
+  //photo sphere viewer
+  var PSV = new PhotoSphereViewer({
+    panorama: 'https://cdn.rawgit.com/mistic100/Photo-Sphere-Viewer/3.1.0/example/Bryce-Canyon-National-Park-Mark-Doliner.jpg',
+    container: 'photosphere',
+    loading_img: 'https://i.stack.imgur.com/MnyxU.gif',
+    navbar: ['autorotate', 'zoom', 'fullscreen','gyroscope'],
+    caption: 'Bryce Canyon National Park <b>&copy; Mark Doliner</b>',
+    default_fov: 65,
+    mousewheel: false
+  })
 
   $(document).ready(function() {
-    //photo sphere viewer
-    var PSV = new PhotoSphereViewer({
-      panorama: 'https://cdn.rawgit.com/mistic100/Photo-Sphere-Viewer/3.1.0/example/Bryce-Canyon-National-Park-Mark-Doliner.jpg',
-      container: 'photosphere',
-      loading_img: 'https://i.stack.imgur.com/MnyxU.gif',
-      navbar: 'autorotate zoom fullscreen',
-      caption: 'Bryce Canyon National Park <b>&copy; Mark Doliner</b>',
-      default_fov: 65,
-      mousewheel: false,
-    })
-    
     //init owl-carousel
     $('.owl-carousel').owlCarousel({
       loop: true,
@@ -45,7 +44,7 @@
       }
     })
 
-   
+
 
     //todo add modernizer on NPM
     //Modernizer detect mobile
@@ -55,11 +54,32 @@
     //fullpage
     $('#fullpage').fullpage({
       //options here
-      scrollingSpeed: 1000,
       verticalCentered: true,
-      menu: '#mainNav'
+      scrollingSpeed: 1000,
+      autoScrolling: true,
+      fitToSection: true,
+      fitToSectionDelay: 2000,
+      menu: '#mainNav',
+      onLeave: function(index, nextIndex, direction) {
+        if (direction == "up") {
+          $(".section").removeClass("down")
+          $(".section").removeClass("next")
+          $(".section").removeClass("prev")
+          $("#fullpage .section:nth-child(" + nextIndex + ")").addClass("up")
+          $("#fullpage .section:nth-child(" + nextIndex + ")").next().addClass("next up")
+          $("#fullpage .section:nth-child(" + nextIndex + ")").prev().addClass("prev up")
+        }
+        else {
+          $(".section").removeClass("up")
+          $(".section").removeClass("next")
+          $(".section").removeClass("prev")
+          $("#fullpage .section:nth-child(" + nextIndex + ")").addClass("down")
+          $("#fullpage .section:nth-child(" + nextIndex + ")").next().addClass("next down")
+          $("#fullpage .section:nth-child(" + nextIndex + ")").prev().addClass("prev down")
+        }
+        console.log(direction + nextIndex)
+      }
     })
-
 
     // Activate scrollspy to add active class to navbar items on scroll
     $('body').scrollspy({
