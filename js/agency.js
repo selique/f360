faeadfa3c89a7fd7c740eacc0519a48f543d1ec1
selique <judeudemolidor@hotmail.com>
2018-09-29 21:24:30 +0000

    modificacoes do projeto (mobile & desktop) faltando animar

--- js/agency.js
+++ js/agency.js
@@ -17,18 +17,13 @@
 
   //photo sphere viewer
   var PSV = new PhotoSphereViewer({
-    panorama: '../img/_claro_prezao_fbiz_panoramica_final_apres.jpg',
+    panorama: '../img/_claro_prezao_fbiz_panoramica_2_optimized-min.jpg',
     container: 'photosphere',
     loading_img: 'https://i.stack.imgur.com/MnyxU.gif',
     default_fov: 50,
     mousewheel: false,
     rotate: true,
-    navbar: [{
-      id: 'my-button',
-      title: 'Hello world',
-      className: 'custom-button',
-      content: '<h1 class="intro__text"><span class="intro__text--main text-uppercase">Soluções em<br>realidade aumentada</span><br><span class="intro__text--secondary text-uppercase">tour virtual, fotos e vídeos em 360 graus</span></h1><a class="intro__btn" href="#portfolio" data-anchor="portfolio"><svg xmlns="http://www.w3.org/2000/svg"><path d="M41.3 1.63l-19.8 19.8L1.7 1.63" /><path d="M41.3 21.63l-19.8 19.8-19.8-19.8" /></a>',
-    }]
+    navbar: false,
   })
 
   $(document).ready(function() {
@@ -36,7 +31,9 @@
     $('.owl-carousel').owlCarousel({
       loop: true,
       margin: 10,
-      nav: true,
+      nav: false,
+      navigation : false,
+      pagination: false,
       responsive: {
         0: {
           items: 1
@@ -51,12 +48,19 @@
     })
 
     // $('video').get(0).play()
-
+    $('video')[0].play();
+    
     //todo add modernizer on NPM
     //Modernizer detect mobile
     //const isMobile = Modernizr.touch;
     //console.log('isMobile: ', isMobile);
 
+    var alturaMenu = $('.navbar').outerHeight() + 'px';
+
+    $(".navbar-nav li a").click(function(event) {
+      $(".navbar-collapse").collapse('hide');
+    });
+    
     //fullpage
     $('#fullpage').fullpage({
       //options here
@@ -64,8 +68,11 @@
       scrollingSpeed: 1000,
       autoScrolling: true,
       fitToSection: true,
+      paddingTop: alturaMenu,
       fitToSectionDelay: 2000,
+      anchors: ['home', 'section2', 'section3', 'footer'],
       menu: '#mainNav',
+      css3: true,
       onLeave: function(index, nextIndex, direction) {
         if (direction == "up") {
           $(".section").removeClass("down")
